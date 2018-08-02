54473728c77fe116fa464365165dcefa77c81b37
Duncan <loxa10077@gmail.com>
2018-08-02 23:11:11 +0800

    attack funtion done

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ class BaseCharacter{
     }
     character.getHurt(damage);
   }
-  getHurt(){
+  getHurt(damage){
     this.hp -= damage;
     if (this.hp <= 0){
       this.die();
@@ -21,29 +21,57 @@ class BaseCharacter{
   die(){
     this.alive = false;
   }
+  updateHtml(hpElement, hurtElement) {
+    hpElement.textContent = this.hp;
+    hurtElement.style.width = (100 - this.hp / this.maxHp * 100) + "%";
+  }
 }
 
 class Hero extends BaseCharacter {
   constructor(name, hp, ap){
     super(name, hp, ap);
+    this.element = document.getElementById("hero-image-block");
+    this.hpElement = document.getElementById("hero-hp");
+    this.maxHpElement = document.getElementById("hero-max-hp");
+    this.hurtElement = document.getElementById("hero-hp-hurt");
+    
+    this.hpElement.textContent = this.hp;
+    this.maxHpElement.textContent = this.maxHp;
+
     console.log('招喚英雄 ' + this.name + '！')
   }
   attack(character){
     var damage = Math.random() * (this.ap / 2) + (this.ap / 2);
     super.attack(character, Math.floor(damage));
   }
+  getHurt(damage) {
+    super.getHurt(damage);
+    this.updateHtml(this.hpElement, this.hurtElement);
+  }
 }
 
 class Monster extends BaseCharacter {
   constructor(name, hp, ap){
     super(name, hp, ap);
+    this.element = document.getElementById("monster-image-block");
+    this.hpElement = document.getElementById("monster-hp");
+    this.maxHpElement = document.getElementById("monster-max-hp");
+    this.hurtElement = document.getElementById("monster-hp-hurt");
+    
+    this.hpElement.textContent = this.hp;
+    this.maxHpElement.textContent = this.maxHp;
+    
     console.log('登登～ ' + (this.name) + ' 出場')
   }
   attack(character){
     var damage = Math.random() * (this.ap / 2) + (this.ap / 2);
     super.attack(character, Math.floor(damage));
   }
+  getHurt(damage) {
+    super.getHurt(damage);
+    this.updateHtml(this.hpElement, this.hurtElement);
+  }
 }
 
 var hero = new Hero('Duncan', 130, 30);
-var monster = new Monster('Monster', 130, 20);
+var monster = new Monster('Monster', 120, 20);
